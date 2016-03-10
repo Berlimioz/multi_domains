064608020d304e9047c576c6fdd7952074b99697
Berlimioz <berlimioz@gmail.com>
2016-03-10 14:36:45 +0100

    remove useless parameters

--- lib/multi_domains.rb
+++ lib/multi_domains.rb
@@ -3,7 +3,7 @@ require 'multi_domains/domain_handler'
 
 module MultiDomains
   class << self
-    attr_accessor :domain_customizable_class, :main_domain_name, :main_domain_extension, :protocol, :force_redirect_to_https, :default_domain
+    attr_accessor :domain_customizable_class, :protocol, :force_redirect_to_https, :default_domain
 
     def force_redirect_to_https?
       force_redirect_to_https
